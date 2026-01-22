9e56804be68d081b7ae817a68b6e9d606062f0cd
Ahmed Ezzat <admedo472@gmail.com>
2026-01-22 21:02:47 +0200

    craete day-6

---
+++ Client-Side/Frameworks/Angular/Tasks/src/app/components/T5/user-api.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-api',
+  imports: [],
+  templateUrl: './user-api.html',
+  styleUrl: './user-api.css',
+})
+export class UserApi {}
